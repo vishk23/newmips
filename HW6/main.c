063c1061af9da1f1ad8f8ea6684763106edf183c
Vishnu Vardhan Kchittibhooma <vishk23@icloud.com>
2022-11-29 12:22:54 -0500

    Initial Commit

---
+++ HW6/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  HW6
+//
+//  Created by Vishnu Vardhan Reddy Kchittibhooma on 11/29/22.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
